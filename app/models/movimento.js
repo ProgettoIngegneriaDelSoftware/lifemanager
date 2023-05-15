a51d4889ad96fba849ebea3d1bd9fd66e0fec342
mauro meneghello <mauro.meneghello@studenti.unitn.it>
2023-05-15 19:22:25 +0200

    api budget non completo

---
+++ app/models/movimento.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// set up a mongoose model
+module.exports = mongoose.model('movimenti', new Schema({  
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'Users'
+    },
+    titolo: {
+        type: String,
+        required: true
+    },
+    importo: {
+        type: Number,
+        required: true
+    },
+    tipologia: {
+        type: String,
+        enum: ['entrata', 'uscita'],
+        required: true
+    },
+    categoria: {
+        type: Schema.Types.String,
+        ref: 'Categoria',
+        //type: String,
+        required: true
+    },
+    note: {
+        type: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+},{ collection: 'movimenti' }));
---
+++ app/movimenti.js
@@ -0,0 +1,131 @@
+const express = require('express');
+const router = express.Router();
+const Movimento = require('./models/movimento'); // get our mongoose model
+//const CategoriaMovimento = require('./models/categoria_movimento'); // get our mongoose model
+
+router.get('', async (req, res) => {
+   
+    let mov = await Movimento.find({ user: req.loggedUser.id });
+    if (!mov) {
+        res.status(404).send()
+        console.log(' movimento not found')
+        return;
+    }
+    mov = mov.map( (movi) => {
+        return {
+            self: '/api/v1/movimenti/' + movi.id,
+            titolo: movi.titolo
+        };
+    });
+    res.status(200).json(mov);
+});
+
+
+router.get('/:id', async (req, res) => {
+    // https://mongoosejs.com/docs/api.html#model_Model.findById
+    let mov = await Movimento.findById(req.params.id);
+    if (!mov) {
+        res.status(404).send()
+        console.log(' movimento not found')
+        return;
+    }
+    res.status(200).json({
+        self: '/api/v1/movimenti/' + mov.id,
+        titolo: mov.titolo,
+        importo: mov.importo,
+        tipologia: mov.tipologia,
+        categoria: mov.categoria,
+        note: mov.note
+    });
+});
+
+
+router.get('/tipologia/:tipologia', async (req, res) => {
+    // https://mongoosejs.com/docs/api.html#model_Model.findById
+    let mov = await Movimento.find({tipologia: req.params.tipologia, user: req.loggedUser.id});
+    if (!mov) {
+        res.status(404).send()
+        console.log(' movimento not found')
+        return;
+    }
+    mov = mov.map( (movi) => {
+        return {
+            self: '/api/v1/movimenti/' + movi.id,
+            titolo: movi.titolo
+        };
+    });
+    res.status(200).json(mov);
+});
+
+
+router.post('', async (req, res) => {
+
+    let categoria = await CategoriaMovimento.findOneAndUpdate(                   //se nell'inserire un movimento l'utente specifica una categoria non presente nel db, viene aggiunta anche tale categoria
+        { nome: req.body.categoria, user: req.loggedUser.id },
+        { nome: req.body.categoria, user: req.loggedUser.id },
+        { upsert: true, new: true }
+    );
+
+	let mov = new Movimento({
+        user: req.loggedUser.id,
+        titolo: req.body.titolo,
+        importo: req.body.importo,
+        tipologia: req.body.tipologia,
+        categoria: categoria.nome,
+        note: req.body.note
+    });
+
+    
+	mov = await mov.save();
+    
+    let movId = mov.id;
+
+    res.location("/api/v1/movimenti/" + movId).status(201).send();
+});
+
+
+router.delete('/:id', async (req, res) => {
+    let mov = await Movimento.findById(req.params.id).exec();
+    if (!mov) {
+        res.status(404).send()
+        console.log('movimento not found')
+        return;
+    }
+    await mov.deleteOne()
+    console.log('movimento removed')
+    res.status(204).send()
+});
+
+
+router.put('/:id', async (req, res) => {
+    try {
+        let categoria = await CategoriaMovimento.findOneAndUpdate(                   //se nel modificare un movimento l'utente specifica una categoria non presente nel db, viene aggiunta anche tale categoria
+            { nome: req.body.categoria, user: req.loggedUser.id },
+            { nome: req.body.categoria, user: req.loggedUser.id },
+            { upsert: true, new: true }
+        );
+
+        let mov = await Movimento.findById(req.params.id);
+
+        if (!mov) {
+            return res.status(404).send();
+        }
+
+        mov.titolo = req.body.titolo;
+        mov.importo = req.body.importo;
+        mov.tipologia = req.body.tipologia;
+        mov.categoria = categoria.nome;
+        mov.note = req.body.note;
+
+        mov = await mov.save();
+
+        res.location("/api/v1/movimenti/" + mov.id).status(201).send();
+    } catch (err) {
+        console.error(err);
+        res.status(500).send();
+    }
+});
+
+
+
+module.exports = router;
