4cde0e0f29a31a82d6d7b5f05ebc0a4b8c715870
lucaboschiero <117825022+lucaboschiero@users.noreply.github.com>
2023-05-21 23:23:51 +0200

    Update app.js

--- app/app.js
+++ app/app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-origin: ['https://lifemanager-frontend.onrender.com/']
+origin: ['https://lifemanager-frontend.onrender.com']
 }));
 
 app.use("/", express.static("static")); // expose also this folder
