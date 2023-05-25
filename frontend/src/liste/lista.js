078f409ebf69d176d65667ae204570d70fdaf3f6
mauro meneghello <mauro.meneghello@studenti.unitn.it>
2023-05-25 21:15:51 +0200

    continuazione frontend liste

--- frontend/src/liste/lista.js
+++ frontend/src/liste/lista.js
@@ -1,72 +1,79 @@
-import React, { useEffect, useCallback, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useCallback, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Lista() {
   const { nomelista } = useParams();
   const [items, setItems] = useState([]);
 
-  const handleCheckboxChange = useCallback((itemId, checked) => {
-    const url = '/api/v1/liste/' + nomelista + '/elementi/' + itemId;
-    const token = localStorage.getItem('token');
+  const handleCheckboxChange = useCallback(
+    (itemId, checked) => {
+      const url = "/api/v1/liste/" + nomelista + "/elementi/" + itemId;
+      const token = localStorage.getItem("token");
 
-    let contrassegnoValue;
-    if (checked) {
+      let contrassegnoValue;
+      if (checked) {
         contrassegnoValue = true; // Imposta a true se checked è true
-    } else {
+      } else {
         contrassegnoValue = false; // Imposta a false se checked è false
-    }
-
-    const requestOptions = {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-access-token': `${token}`
-      },
-      body: JSON.stringify({
-        contrassegno: contrassegnoValue
-      })
-    };
-  
-    fetch(url, requestOptions)
-      .then((response) => {
-        if (response.ok) {
-          // Aggiornamento del campo "contrassegno" nel database avvenuto con successo
-        } else {
-          throw new Error('Error: ' + response.status);
-        }
-      })
-      .catch((error) => {
-        console.error(error); // Gestisci gli errori
-      });
-  }, [nomelista]);
-
-  useEffect(() => {
-    const url = '/api/v1/liste/' + nomelista + '/elementi';
-    const token = localStorage.getItem('token');
-
-    const requestOptions = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-access-token': `${token}`
       }
-    };
 
-    fetch(url, requestOptions)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Error: ' + response.status);
-        }
-      })
-      .then((data) => {
-        setItems(data.items); // Imposta lo stato `items` con i dati ottenuti dalla richiesta
-      })
-      .catch((error) => {
-        console.error(error); // Gestisci gli errori
-      });
-  }, [nomelista], items);
+      const requestOptions = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": `${token}`,
+        },
+        body: JSON.stringify({
+          contrassegno: contrassegnoValue,
+        }),
+      };
+
+      fetch(url, requestOptions)
+        .then((response) => {
+          if (response.ok) {
+            // Aggiornamento del campo "contrassegno" nel database avvenuto con successo
+          } else {
+            throw new Error("Error: " + response.status);
+          }
+        })
+        .catch((error) => {
+          console.error(error); // Gestisci gli errori
+        });
+    },
+    [nomelista]
+  );
+
+  useEffect(
+    () => {
+      const url = "/api/v1/liste/" + nomelista + "/elementi";
+      const token = localStorage.getItem("token");
+
+      const requestOptions = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": `${token}`,
+        },
+      };
+
+      fetch(url, requestOptions)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("Error: " + response.status);
+          }
+        })
+        .then((data) => {
+          setItems(data.items); // Imposta lo stato `items` con i dati ottenuti dalla richiesta
+        })
+        .catch((error) => {
+          console.error(error); // Gestisci gli errori
+        });
+    },
+    [nomelista],
+    items
+  );
 
   const handleCheckboxChangeItem = (itemId, checked) => {
     const updatedItems = items.map((item) => {
@@ -75,11 +82,11 @@ function Lista() {
       }
       return item;
     });
-  
+
     setItems(updatedItems);
     handleCheckboxChange(itemId, checked); // Chiamata alla funzione handleCheckboxChange per salvare nel database
   };
-  
+
   const renderedItems = items.map((item) => {
     return (
       <div key={item._id}>
@@ -93,13 +100,15 @@ function Lista() {
         <hr />
       </div>
     );
-  });  
+  });
 
   return (
     <>
       <h2>{nomelista}</h2>
       <div id="result">{renderedItems}</div>
-      <a href={`/${nomelista}/nuovoelemento`}><button>Nuovo Elemento</button></a>
+      <a href={`/${nomelista}/nuovoelemento`}>
+        <button>Nuovo Elemento</button>
+      </a>
     </>
   );
 }
--- frontend/src/liste/nuovoelemento.js
+++ frontend/src/liste/nuovoelemento.js
@@ -1,42 +1,53 @@
-import React, { useState } from 'react';
-import { useNavigate, useParams} from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 function NuovoElemento() {
   const { nomelista } = useParams();
-  const [formData, setFormData] = useState({
-    nome: '',
-  });
+  const [formData, setFormData] = useState([{ nome: "" }]);
 
-  const handleInputChange = (event) => {
-    setFormData({ ...formData, [event.target.name]: event.target.value });
+  const handleInputChange = (event, index) => {
+    const value = event.target.value;
+    const updatedData = [...formData];
+    updatedData[index] = { nome: value };
+    setFormData(updatedData);
   };
 
   const navigate = useNavigate();
 
+  const handleAggiungiElem = () => {
+    setFormData([...formData, { nome: "" }]);
+  };
+
+  const handleRimuoviElem = (index) => {
+    const updatedData = [...formData];
+    updatedData.splice(index, 1);
+    setFormData(updatedData);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
 
-    const url = '/api/v1/liste/' + nomelista + '/elementi';
+    const url = "/api/v1/liste/" + nomelista + "/elementi";
     const requestOptions = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'x-access-token' : `${token}`
+        "Content-Type": "application/json",
+        "x-access-token": `${token}`,
       },
       body: JSON.stringify({
-        items: [formData.nome]
-      })
+        items: formData.map((item) => item.nome),
+      }),
     };
 
     fetch(url, requestOptions)
       .then((response) => {
         if (response.ok) {
           // Se la risposta è OK, reindirizza a un'altra pagina
-          navigate('/liste/'+nomelista);
+          navigate("/liste/" + nomelista);
         } else {
           // Se la risposta non è OK, gestisci l'errore
-          throw new Error('Error: ' + response.status);
+          throw new Error("Error: " + response.status);
         }
       })
       .catch((error) => {
@@ -47,16 +58,32 @@ function NuovoElemento() {
   return (
     <>
       <h2>Nuovo Elemento</h2>
-      
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label> Nome:
-            <input id="nome" type="text" name="nome" value={formData.nome} onChange={handleInputChange}/>
-          </label>
-          <br></br>
-          <button type="submit">Aggiungi Elemento</button>
-        </form>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <h4>Nome:</h4>
+          {formData.map((elem, index) => (
+            <div key={index}>
+              <input
+                type="text"
+                name="nome"
+                placeholder="Nome elemento"
+                onChange={(event) => handleInputChange(event, index)}
+                required
+              />
+              {index > 0 && (
+                <button type="button" onClick={() => handleRimuoviElem(index)}>
+                  Rimuovi
+                </button>
+              )}
+            </div>
+          ))}
+          <button type="button" onClick={handleAggiungiElem}>
+            Aggiungi elemento da aggiungere
+          </button>
+        </div>
+        <br></br>
+        <button type="submit">Aggiungi Elemento</button>
+      </form>
     </>
   );
 }
