4b04a6608f84be852e6e79c7a6e0728454847522
Nicola Turniano <nicola.turniano01@gmail.com>
2023-05-26 09:34:29 +0200

    end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,41 @@
-import "./App.css";
+import './App.css';
 // import ProtectedRoute from './ProtectedRoute';
-import Registrati from "./autenticazione/registrazione";
-import Login from "./autenticazione/login";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Registrati from './autenticazione/registrazione';
+import Login from './autenticazione/login';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-import Liste from "./liste/liste";
-import NuovaLista from "./liste/nuovalista";
-import Lista from "./liste/lista";
-import NuovoElemento from "./liste/nuovoelemento";
+import Liste from './liste/liste';
+import NuovaLista from './liste/nuovalista'
+import Lista from './liste/lista';
+import NuovoElemento from './liste/nuovoelemento'
 
-import Ricette from "./ricette/ricette";
-import NuovaRicetta from "./ricette/nuovaricetta";
+import Ricette from './ricette/ricette'
+import NuovaRicetta from './ricette/nuovaricetta'
+import SingolaRicetta from './ricette/singolaricetta'
+
 import Movimenti from './movimenti/movimenti';
 import NuovoMovimento from './movimenti/nuovomovimento';
 import VisualizzaMovimento from './movimenti/visualizzamovimento';
 import EliminaMovimento from './movimenti/eliminamovimento';
 
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Outlet,
-} from "react-router-dom";
-import Homepage from "./components/Homepage";
-import SingolaRicetta from "./ricette/singolaricetta";
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import Homepage from './components/Homepage';
+import ModificaMovimento from './movimenti/modificamovimento';
+import VisualizzaPerCategorie from './movimenti/visualizzapercategorie';
+import Entrate from './movimenti/Entrate';
+import Uscite from './movimenti/Uscite';
+
 
 function App() {
+
+  let userInfo = {
+    id: '',
+    email: '',
+    token: '',
+    message: 'hjjh'
+  }
+
   const Layout = () => {
     return (
       <>
@@ -34,8 +43,8 @@ function App() {
         <Outlet />
         <Footer />
       </>
-    );
-  };
+    )
+  }
 
   return (
     <div>
@@ -60,6 +69,15 @@ function App() {
               path="/ricette/:nomericetta"
               element={<SingolaRicetta />}
             />
+            <Route exact path="/movimenti" element={<Movimenti />} />
+            <Route exact path="/NuovoMovimento" element={<NuovoMovimento />} />
+            <Route path="/eliminamovimento/:id" element={<EliminaMovimento />} />
+            <Route path="/visualizzamovimento/:id" element={<VisualizzaMovimento />} />
+            <Route path="/modificamovimento/:id" element={<ModificaMovimento />} />
+            <Route path="/visualizzapercategorie/:nome" element={<VisualizzaPerCategorie />} />
+            <Route path="/Entrate" element={<Entrate />} />
+            <Route path="/Uscite" element={<Uscite />} />
+
           </Route>
         </Routes>
       </Router>
