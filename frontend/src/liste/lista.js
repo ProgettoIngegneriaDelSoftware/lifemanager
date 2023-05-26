5b673deab8b42d883ca19cc081b3f3d214553417
mauro meneghello <mauro.meneghello@studenti.unitn.it>
2023-05-26 15:37:57 +0200

    modifiche frontend liste

--- frontend/src/liste/lista.js
+++ frontend/src/liste/lista.js
@@ -1,79 +1,102 @@
-import React, { useEffect, useCallback, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Lista() {
   const { nomelista } = useParams();
   const [items, setItems] = useState([]);
 
-  const handleCheckboxChange = useCallback(
-    (itemId, checked) => {
-      const url = "/api/v1/liste/" + nomelista + "/elementi/" + itemId;
-      const token = localStorage.getItem("token");
+  const handleCheckboxChange = (itemId, checked) => {
+    const url = "/api/v1/liste/" + nomelista + "/elementi/" + itemId;
+    const token = localStorage.getItem("token");
 
-      let contrassegnoValue;
-      if (checked) {
-        contrassegnoValue = true; // Imposta a true se checked è true
-      } else {
-        contrassegnoValue = false; // Imposta a false se checked è false
-      }
+    let contrassegnoValue;
+    if (checked) {
+      contrassegnoValue = true; // Imposta a true se checked è true
+    } else {
+      contrassegnoValue = false; // Imposta a false se checked è false
+    }
 
-      const requestOptions = {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          "x-access-token": `${token}`,
-        },
-        body: JSON.stringify({
-          contrassegno: contrassegnoValue,
-        }),
-      };
+    const requestOptions = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": `${token}`,
+      },
+      body: JSON.stringify({
+        contrassegno: contrassegnoValue,
+      }),
+    };
 
-      fetch(url, requestOptions)
-        .then((response) => {
-          if (response.ok) {
-            // Aggiornamento del campo "contrassegno" nel database avvenuto con successo
-          } else {
-            throw new Error("Error: " + response.status);
-          }
-        })
-        .catch((error) => {
-          console.error(error); // Gestisci gli errori
-        });
-    },
-    [nomelista]
-  );
+    fetch(url, requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          // Aggiornamento del campo "contrassegno" nel database avvenuto con successo
+          console.log("check modificato");
+        } else {
+          throw new Error("Error: " + response.status);
+        }
+      })
+      .catch((error) => {
+        console.error(error); // Gestisci gli errori
+      });
+  };
 
-  useEffect(
-    () => {
-      const url = "/api/v1/liste/" + nomelista + "/elementi";
-      const token = localStorage.getItem("token");
+  const handleElimina = (item) => {
+    const url = "/api/v1/liste/" + nomelista + "/elementi/" + item._id;
+    const token = localStorage.getItem("token");
 
-      const requestOptions = {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          "x-access-token": `${token}`,
-        },
-      };
+    const requestOptions = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": `${token}`,
+      },
+    };
+    fetch(url, requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          console.log("Elemento eliminato");
+          setItems((prevItems) =>
+            prevItems.filter((elem) => elem.nome !== item.nome)
+          );
+          return;
+        } else {
+          throw new Error("Error: " + response.status);
+        }
+      })
+      .then(() => {})
+      .catch((error) => {
+        console.error(error); // Gestisci gli errori
+      });
+  };
 
-      fetch(url, requestOptions)
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error("Error: " + response.status);
-          }
-        })
-        .then((data) => {
-          setItems(data.items); // Imposta lo stato `items` con i dati ottenuti dalla richiesta
-        })
-        .catch((error) => {
-          console.error(error); // Gestisci gli errori
-        });
-    },
-    [nomelista],
-    items
-  );
+  useEffect(() => {
+    const url = "/api/v1/liste/" + nomelista + "/elementi";
+    const token = localStorage.getItem("token");
+
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": `${token}`,
+      },
+    };
+
+    fetch(url, requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Error: " + response.status);
+        }
+      })
+      .then((data) => {
+        setItems(data.items); // Imposta lo stato `items` con i dati ottenuti dalla richiesta
+      })
+      .catch((error) => {
+        console.error(error); // Gestisci gli errori
+      });
+  }, [nomelista]);
 
   const handleCheckboxChangeItem = (itemId, checked) => {
     const updatedItems = items.map((item) => {
@@ -84,7 +107,10 @@ function Lista() {
     });
 
     setItems(updatedItems);
-    handleCheckboxChange(itemId, checked); // Chiamata alla funzione handleCheckboxChange per salvare nel database
+
+    const contrassegnoValue = checked ? true : false;
+
+    handleCheckboxChange(itemId, contrassegnoValue); // Chiamata alla funzione handleCheckboxChange per salvare nel database
   };
 
   const renderedItems = items.map((item) => {
@@ -96,6 +122,11 @@ function Lista() {
           onChange={(e) => handleCheckboxChangeItem(item._id, e.target.checked)}
         />
         <span>Nome: {item.nome}</span>
+        <span>
+          <button value={item.nome} onClick={() => handleElimina(item)}>
+            Elimina
+          </button>
+        </span>
         <br />
         <hr />
       </div>
--- frontend/src/liste/liste.js
+++ frontend/src/liste/liste.js
@@ -1,19 +1,47 @@
-import React, { useEffect, useState } from "react";
-import { OverlayTrigger, Popover, Button } from 'react-bootstrap';
+/*import React, { useEffect, useState } from "react";
+import { OverlayTrigger, Popover, Button } from "react-bootstrap";
 
 function Liste() {
-  const url = '/api/v1/liste/';
-  const token = localStorage.getItem('token');
+  const url = "/api/v1/liste/";
+  const token = localStorage.getItem("token");
   const [liste, setListe] = useState([]);
 
+  const handleElimina = (item) => {
+    const url = "/api/v1/liste/" + item;
+    const token = localStorage.getItem("token");
+
+    const requestOptions = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": `${token}`,
+      },
+    };
+    fetch(url, requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          console.log("Lista eliminata");
+          setListe((prevListe) =>
+            prevListe.filter((elem) => elem.nome !== item)
+          );
+          return;
+        } else {
+          throw new Error("Error: " + response.status);
+        }
+      })
+      .then(() => {})
+      .catch((error) => {
+        console.error(error); // Gestisci gli errori
+      });
+  };
 
   useEffect(() => {
     const requestOptions = {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-        'x-access-token': `${token}`
-      }
+        "Content-Type": "application/json",
+        "x-access-token": `${token}`,
+      },
     };
 
     fetch(url, requestOptions)
@@ -21,7 +49,7 @@ function Liste() {
         if (response.ok) {
           return response.json();
         } else {
-          throw new Error('Error: ' + response.status);
+          throw new Error("Error: " + response.status);
         }
       })
       .then((data) => {
@@ -34,28 +62,42 @@ function Liste() {
 
   return (
     <>
-      <div class='listeContainer'>
+      <div class="listeContainer">
         <h2>Liste</h2> <br />
-
         {liste.map((element, index) => (
           <p key={index}>
-            <div className="input-group" style={{ display: 'flex' }}>
-              <a href={`/liste/${element.nome}`} style={{ textDecoration: 'none' }}>
-                <button className=" form-control " style={{ width: '500px' }}>{element.nome}</button> </a>
+            <div className="input-group" style={{ display: "flex" }}>
+              <a
+                href={`/liste/${element.nome}`}
+                style={{ textDecoration: "none" }}
+              >
+                <button className=" form-control " style={{ width: "500px" }}>
+                  {element.nome}
+                </button>{" "}
+              </a>
               <OverlayTrigger
                 trigger="click"
                 placement="top"
                 overlay={
                   <Popover id="popover-menu">
                     <Popover.Body>
-                      <Button variant="danger">Elimina</Button>{' '}
+                      <Button
+                        variant="danger"
+                        onClick={() => handleElimina(element.nome)}
+                      >
+                        Elimina
+                      </Button>{" "}
                       <Button variant="primary">Modifica</Button>
                     </Popover.Body>
                   </Popover>
                 }
               >
                 <span className="input-group-text">
-                  <img src={process.env.PUBLIC_URL + '/trepunti.png'} alt="" className="trePunti" />
+                  <img
+                    src={process.env.PUBLIC_URL + "/trepunti.png"}
+                    alt=""
+                    className="trePunti"
+                  />
                 </span>
               </OverlayTrigger>
 
@@ -67,8 +109,195 @@ function Liste() {
           </p>
         ))}
         <br />
-        <a href="/nuovalista"><button>Nuova Lista</button></a>
-      </div >
+        <a href="/nuovalista">
+          <button>Nuova Lista</button>
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default Liste;*/
+import React, { useEffect, useState } from "react";
+import { OverlayTrigger, Popover, Button } from "react-bootstrap";
+
+function Liste() {
+  const url = "/api/v1/liste/";
+  const token = localStorage.getItem("token");
+  const [liste, setListe] = useState([]);
+  const [editingList, setEditingList] = useState("");
+  const [newListName, setNewListName] = useState("");
+
+  const handleElimina = (item) => {
+    const deleteUrl = "/api/v1/liste/" + item;
+    const requestOptions = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": `${token}`,
+      },
+    };
+    fetch(deleteUrl, requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          console.log("Lista eliminata");
+          setListe((prevListe) =>
+            prevListe.filter((elem) => elem.nome !== item)
+          );
+        } else {
+          throw new Error("Error: " + response.status);
+        }
+      })
+      .catch((error) => {
+        console.error(error); // Gestisci gli errori
+      });
+  };
+
+  const handleModifica = (item) => {
+    setEditingList(item);
+    setNewListName(item);
+  };
+
+  const handleSaveModifica = () => {
+    // Implementa la logica per salvare la modifica del nome della lista
+    // Puoi utilizzare la variabile "newListName" per ottenere il nuovo nome
+    const url = "/api/v1/liste/" + editingList;
+    const token = localStorage.getItem("token");
+
+    const requestOptions = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": `${token}`,
+      },
+      body: JSON.stringify({
+        nome: newListName,
+      }),
+    };
+
+    fetch(url, requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          console.log("Lista modificata");
+          // Puoi eseguire un'azione aggiuntiva dopo la modifica, come il reindirizzamento a un'altra pagina
+        } else {
+          throw new Error("Error: " + response.status);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    console.log("Salva modifica lista: ", newListName);
+
+    // Aggiorna lo stato delle liste con il nuovo nome modificato
+    setListe((prevListe) =>
+      prevListe.map((elem) =>
+        elem.nome === editingList ? { ...elem, nome: newListName } : elem
+      )
+    );
+
+    // Resetta le variabili di editing
+    setEditingList("");
+    setNewListName("");
+  };
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": `${token}`,
+      },
+    };
+
+    fetch(url, requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Error: " + response.status);
+        }
+      })
+      .then((data) => {
+        setListe(data);
+      })
+      .catch((error) => {
+        console.error(error); // Gestisci gli errori
+      });
+  }, [url, token]);
+
+  return (
+    <>
+      <div className="listeContainer">
+        <h2>Liste</h2> <br />
+        {liste.map((element, index) => (
+          <p key={index}>
+            <div className="input-group" style={{ display: "flex" }}>
+              {editingList === element.nome ? (
+                <input
+                  type="text"
+                  value={newListName}
+                  onChange={(e) => setNewListName(e.target.value)}
+                />
+              ) : (
+                <a
+                  href={`/liste/${element.nome}`}
+                  style={{ textDecoration: "none" }}
+                >
+                  <button className="form-control" style={{ width: "500px" }}>
+                    {element.nome}
+                  </button>{" "}
+                </a>
+              )}
+              <OverlayTrigger
+                trigger="click"
+                placement="top"
+                overlay={
+                  <Popover id="popover-menu">
+                    <Popover.Body>
+                      <Button
+                        variant="danger"
+                        onClick={() => handleElimina(element.nome)}
+                      >
+                        Elimina
+                      </Button>{" "}
+                      {editingList === element.nome ? (
+                        <Button variant="primary" onClick={handleSaveModifica}>
+                          Salva
+                        </Button>
+                      ) : (
+                        <Button
+                          variant="primary"
+                          onClick={() => handleModifica(element.nome)}
+                        >
+                          Modifica
+                        </Button>
+                      )}
+                    </Popover.Body>
+                  </Popover>
+                }
+              >
+                <span className="input-group-text">
+                  <img
+                    src={process.env.PUBLIC_URL + "/trepunti.png"}
+                    alt=""
+                    className="trePunti"
+                  />
+                </span>
+              </OverlayTrigger>
+
+              <div id="menu" className="menu">
+                <button>Modifica</button>
+                <button>Elimina</button>
+              </div>
+            </div>
+          </p>
+        ))}
+        <br />
+        <a href="/nuovalista">
+          <button>Nuova Lista</button>
+        </a>
+      </div>
     </>
   );
 }
