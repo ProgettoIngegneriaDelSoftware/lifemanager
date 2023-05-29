9009edb45ded92e251cf5e67c7861b2c6bfdf048
lucaboschiero <117825022+lucaboschiero@users.noreply.github.com>
2023-05-29 13:24:25 +0200

    Delete config.js

--- backend/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    frontendUrl: 'http://localhost:3000',
-    email: 'piertech10@gmail.com',
-    password: 'sidmldrctmhrhlcq',
-    // Altre variabili di configurazione
-};
-
-module.exports = config;
