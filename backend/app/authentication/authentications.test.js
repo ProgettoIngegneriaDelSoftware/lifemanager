23ee4487ec22fbc9332b31e36ddb9244cdb5a2bb
mauro meneghello <mauro.meneghello@studenti.unitn.it>
2023-06-01 21:49:18 +0200

    test login

--- backend/app/authentication/authentications.test.js
+++ backend/app/authentication/authentications.test.js
@@ -3,7 +3,7 @@ const app = require("../app");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 
-describe("GET /api/v1/authentications", () => {
+describe("POST /api/v1/authentications", () => {
   beforeAll(async () => {
     jest.setTimeout(8000);
     app.locals.db = await mongoose.connect(process.env.MONGODB_URL);
@@ -16,20 +16,60 @@ describe("GET /api/v1/authentications", () => {
     expiresIn: 86400,
   }); // create a valid token
 */
-  const requestBody = {
+  const requestBody1 = {
     username: "mm",
     password: "wrongPassword",
   };
-  test("POST /api/v1/authentications with wrong password", () => {
+  test("POST /api/v1/authentications Login di un utente registrato con username o email correti ma password sbagliata", () => {
     return (
       request(app)
         .post("/api/v1/authentications")
         //.set("x-access-token", token)
         .set("Accept", "application/json")
-        .send(requestBody)
+        .send(requestBody1)
         .expect({
           success: false,
           message: "Authentication failed. Wrong password.",
+        })
+    );
+  });
+
+  const requestBody2 = {
+    username: "mmmm",
+    password: "wrongPassword",
+  };
+  test("POST /api/v1/authentications Login di un utente registrato con email o username non corretti", () => {
+    return (
+      request(app)
+        .post("/api/v1/authentications")
+        //.set("x-access-token", token)
+        .set("Accept", "application/json")
+        .send(requestBody2)
+        .expect({
+          success: false,
+          message: "Authentication failed. User not found.",
+        })
+    );
+  });
+
+  const requestBody3 = {
+    username: "mm",
+    password: "ProvaProva1.",
+  };
+  test("POST /api/v1/authentications Login di un utente registrato con email o username e password validi", () => {
+    return (
+      request(app)
+        .post("/api/v1/authentications")
+        //.set("x-access-token", token)
+        .set("Accept", "application/json")
+        .send(requestBody3)
+        .expect((res) => {
+          expect(res.body.success).toBe(true);
+          expect(res.body.message).toBe("Enjoy your token!");
+          expect(res.body.token).toBeDefined();
+          expect(res.body.email).toBeDefined();
+          expect(res.body.id).toBeDefined();
+          expect(res.body.self).toBeDefined();
         })
     );
   });
